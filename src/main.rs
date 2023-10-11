79b999ed35d4db99134622875f902ed04b6d5f23
chocological00 <36347199+chocological00@users.noreply.github.com>
2023-10-11 18:01:26 -0400

    axum boilerplates

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,25 @@
 #![deny(clippy::panic)]
 #![deny(clippy::indexing_slicing)]
 
-fn main() {
-	println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing::get, Router, Server};
+use tracing::*;
+
+#[tokio::main]
+async fn main() {
+	dotenvy::dotenv().ok();
+
+	tracing_subscriber::fmt::init();
+
+	let app = Router::new().route("/", get(root));
+
+	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+	if let Err(e) = Server::bind(&addr).serve(app.into_make_service()).await {
+		error!("server error: {}", e);
+	}
+}
+
+async fn root() -> impl IntoResponse {
+	"Hello, World!"
 }
