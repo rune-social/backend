8677bb0d3b25aaa1078d7f41a6ac6a908e212475
chocological00 <36347199+chocological00@users.noreply.github.com>
2023-10-11 17:11:26 -0400

    cargo init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![deny(unsafe_code)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::indexing_slicing)]
+
+fn main() {
+	println!("Hello, world!");
+}
